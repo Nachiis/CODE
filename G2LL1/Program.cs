51c4e3c702ec409deeaa70a10360ba4b55f4d42c
Nachiis <272976777@qq.com>
2025-12-03 14:21:25 +0800

    文法转LL1

---
+++ G2LL1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace G2LL1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            System.Console.WriteLine($"args count:{args.Length}");
+            foreach (var arg in args)
+            {
+                System.Console.WriteLine($"arg:{arg}");
+            }
+        }
+    }
+}
