0bfe57a9a196d4dfa1b751ea9c07419398dc9de8
Nachiis <272976777@qq.com>
2025-12-06 15:55:59 +0800

    计算First集

---
+++ G2LL1/FirstCalculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G2LL1
+{
+    using FirstSet = Dictionary<string, HashSet<string>>;
+    internal class FirstCalculator
+    {
+        public static FirstSet CalcFirstSet(Grammar grammar)
+        {
+            var FIRST = new FirstSet();
+            foreach(var terminal in grammar.Terminals)
+            {
+                FIRST[terminal] = new HashSet<string> { terminal };
+            }
+            foreach(var variable in grammar.Variables)
+            {
+                FIRST[variable] = new HashSet<string>();
+            }
+            FIRST[""] = new HashSet<string> { "" };
+            bool changed = true;
+            while (changed)
+            {
+                changed = false;
+                foreach (var (left,rights) in grammar.Productions)
+                {
+                    foreach(var right in rights)
+                    { 
+                        // 空
+                        if(right.Count == 1 && right[0] == "")
+                        {
+                            
+                            if (!FIRST[left].Contains(""))
+                            {
+                                FIRST[left].Add("");
+                                changed = true;
+                            }
+                            continue;
+                        }
+                        // A-> X1 X2 ... Xn
+                        for (int i = 0; i < right.Count; i++)
+                        {
+                            var symbol = right[i];
+                            foreach (var firstSymbol in FIRST[symbol])
+                            {
+                                if (firstSymbol != "" && FIRST[left].Add(firstSymbol))
+                                {
+                                    changed = true;
+                                }
+                            }
+                            if (!FIRST[symbol].Contains(""))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                // X1 X2 ... Xn 全部能推导出 ε
+                                if (i == right.Count - 1)
+                                {
+                                    if (FIRST[left].Add(""))
+                                    {
+                                        changed = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return FIRST;
+        }
+        public static string FirstSetToString(FirstSet firstSet)
+        {
+            StringBuilder sb = new();
+            foreach (var (symbol, firsts) in firstSet)
+            {
+                sb.AppendLine($"FIRST({symbol}) = {{ {string.Join(", ", firsts)} }}");
+            }
+            return sb.ToString();
+        }
+    }
+}
